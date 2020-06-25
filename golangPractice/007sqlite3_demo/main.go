d1fea4f92d5833b1fe0fccfa0f07af0476aef81c
yes5144 <1006793841@qq.com>
2020-06-25 11:08:04 +0800

    sqlite3

---
+++ golangPractice/007sqlite3_demo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+
+	//插入数据
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+}
+
+// 链接：https://www.jianshu.com/p/5e1a8fe54393
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalf("err, Failed code %#v", err)
+	}
+}
